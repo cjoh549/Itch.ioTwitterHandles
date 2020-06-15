d3b1f30a74ac80e2b2b2e47a97a48bce6030d841
cjoh549 <cjoh549@wgu.edu>
2020-06-15 10:54:02 -0700

    Created file

---
+++ itchio.py
@@ -0,0 +1,35 @@
+import json, re, os
+import urllib.request
+import requests, bs4
+
+# Grab the json file and load it
+itch_json = urllib.request.urlopen("https://itch.io/bundle/520/participants.json").read(500000)
+
+itch = json.loads(itch_json)
+
+# loop through the participants
+for x in itch['participants']:
+
+    # get the page url
+    url = x['url']
+
+    res = requests.get(url)
+
+    page = bs4.BeautifulSoup(res.text, "html.parser")
+
+    # find all the links that are the same format as the twitter link
+    elms = page.find_all('a', rel="me")
+
+    tmp_file = open(os.getcwd() + '/twitterhandles.txt', 'a')
+
+    for tmatch in elms:
+
+        # use a regex to find a twitter handle and print it out
+        check_regex = re.compile("@([A-Za-z0-9_]+)")
+        check_result = check_regex.match(tmatch.text)
+        
+        if check_result != None:
+            tmp_file.write(tmatch.text + "\n")
+            print(tmatch.text)
+
+tmp_file.close()
